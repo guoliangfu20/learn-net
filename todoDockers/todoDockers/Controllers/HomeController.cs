7deadf8555351dbfcab8319e0bb34eb49031a081
fuguoliang <fglone@126.com>
2023-07-17 09:02:22 +0800

    717

---
+++ todoDockers/todoDockers/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todoDockers.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ todoDockers/todoDockers/Controllers/TodoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todoDockers.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["oneName"] = "GuoliangFu";
+            return View();
+        }
+    }
+}
