e00410518adaa35e5c7dfee2bf06c55433e7d71d
fuguoliang <fglone@126.com>
2023-12-20 21:47:06 +0800

    1

---
+++ LearnsTest/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+List<Order> orders = new List<Order>
+{
+    new Order { Id = 1001, Name="aaaa"},
+    new Order { Id = 1003, Name="bbbb"},
+    new Order { Id = 1006, Name="cc"},
+    new Order { Id = 1110, Name="f"},
+};
+
+List<string> fields = typeof(Order).GetProperties().Select(s => s.Name).ToList();
+
+Console.WriteLine(string.Join(",",fields));
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+
---
+++ LearnsTest/shuzu/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+List<Person> persons = new List<Person>()
+{
+    new Person(){  id=1001,name="aaaa"},
+    new Person(){  id=1002,name="bbb"},
+    new Person(){  id=1003,name="ccc"},
+    new Person(){  id=1004,name="ddd"},
+};
+
+List<int> ints = new List<int>() { 1001, 1003, 1005 };
+
+string dels = "";
+foreach (var item in ints)
+{
+    if (!persons.Exists(p => p.id == item))
+    {
+        dels += item;
+    }
+
+}
+
+
+public class Person
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int isOK { get; set; }
+}
+
+
